51f534c8a99bcae5393feb823f79eede04332ea2
Hashmat Ali <hashmatali1431@gmail.com>
2026-02-14 14:29:07 +0500

    url api updatred and fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://digital-gateway-b.onrender.com/',
+  apiUrl: 'https://digital-gateway-b.onrender.com',
   allowedRoles: {
     admin: ['admin'],
     counselor: ['admin', 'counselor'],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://digital-gateway-b.onrender.com/'
+  apiUrl: 'https://digital-gateway-b.onrender.com'
 };
